9433074a4e4135c9696a7a7e700bbdf6272b6d94
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2012-01-16 19:12:36 -0700

    Adding dataIO module. Starting with df2dbf and dbf2df

---
+++ pyGDsandbox/dataIO.py
@@ -0,0 +1,83 @@
+'''
+dataIO: module for code related to data files manipulation
+
+Find here classes and functions to deal with DBFs, CSVs as well as Numpy arrays,
+pandas DataFrames, etc.
+'''
+
+import pysal as ps
+import numpy as np
+import pandas
+
+def df2dbf(df, dbf_path, my_specs=None):
+    '''
+    Convert a pandas.DataFrame into a dbf. 
+
+    __author__  = "Dani Arribas-Bel <darribas@asu.edu> "
+    ...
+
+    Arguments
+    ---------
+    df          : DataFrame
+                  Pandas dataframe object to be entirely written out to a dbf
+    dbf_path    : str
+                  Path to the output dbf. It is also returned by the function
+    my_specs    : list
+                  List with the field_specs to use for each column.
+                  Defaults to None and applies the following scheme:
+                    * int: ('N', 14, 0)
+                    * float: ('N', 14, 14)
+                    * str: ('C', 14, 0)
+    '''
+    if my_specs:
+        specs = my_specs
+    else:
+        type2spec = {int: ('N', 20, 0),
+                float: ('N', 36, 14),
+                str: ('C', 14, 0)
+                }
+        types = [type(df[i][0]) for i in df.columns]
+        specs = [type2spec[t] for t in types]
+    db = ps.open(dbf_path, 'w')
+    db.header = list(df.columns)
+    db.field_spec = specs
+    for i, row in df.T.iteritems():
+        db.write(row)
+    db.close()
+    return dbf_path
+
+def dbf2df(dbf_path, index=None, cols=False):
+    '''
+    Read a dbf file as a pandas.DataFrame, optionally selecting the index
+    variable and which columns are to be loaded.
+
+    __author__  = "Dani Arribas-Bel <darribas@asu.edu> "
+    ...
+
+    Arguments
+    ---------
+    dbf_path    : str
+                  Path to the DBF file to be read
+    index       : str
+                  Name of the column to be used as the index of the DataFrame
+    cols        : list
+                  List with the names of the columns to be read into the
+                  DataFrame. Defaults to False, which reads the whole dbf
+
+    Returns
+    -------
+    df          : DataFrame
+                  pandas.DataFrame object created
+    '''
+    db = ps.open(dbf_path)
+    if cols:
+        vars_to_read = cols
+    else:
+        vars_to_read = db.header
+    data = dict([(var, db.by_col(var)) for var in vars_to_read])
+    db.close()
+    if index:
+        return pandas.DataFrame(data, index=data[index])
+    else:
+        return pandas.DataFrame(data)
+
